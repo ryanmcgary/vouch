97c656f32919b9953c981beb3e9c2a9be2e1d634
Ryan <asheepingreen@hotmail.com>
2011-02-27 14:17:08 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vouch::Application.config.secret_token = '4467b92f9354fa4c049dc0d0b20abd04614d8405f15c773bbf00d356c526866e0d0c337435380db3721c1e743ea67f457e995dc541b6a8b19f7b8e683f214bd1'
